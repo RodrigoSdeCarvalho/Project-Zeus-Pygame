b36bc4606c39a400ea50640f50c30a3b556024e8
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2022-01-20 16:50:12 -0300

    Criação da classe Game e função open_menu

---
+++ prototipo/Game.py
@@ -0,0 +1,20 @@
+import pygame
+from GameLogic.GUI.GameDisplay import GameDisplay
+from Menu.MainMenu import MainMenu
+
+pygame.init()
+pygame.display.set_caption("Project Zeus")
+pygame.display.set_icon(pygame.transform.scale(pygame.image.load('prototipo/Images/game_icon.png'), (75,75)))
+
+class Game:
+    def __init__(self):
+        self.fps = 30
+        self.clock = pygame.time.Clock()
+        self.display = GameDisplay()
+
+    def open_menu(self):
+        mainMenu = MainMenu(self.display)
+        mainMenu.show_main_menu()
+
+game = Game()
+game.open_menu()
