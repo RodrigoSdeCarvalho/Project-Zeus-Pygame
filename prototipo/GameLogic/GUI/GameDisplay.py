1dc84062cfbd18cd18bde38f6771d8f595da96b6
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2022-01-20 16:51:02 -0300

    Criação da classe GameDisplay (tela do pygame)

---
+++ prototipo/GameLogic/GUI/GameDisplay.py
@@ -0,0 +1,28 @@
+import pygame
+
+class GameDisplay:
+    def __init__(self):
+        self.__width = 800
+        self.__height = 600
+        self.__display = pygame.display.set_mode((self.__width, self.__height))
+        #self.__display_background = pygame.transform.scale(pygame.image.load('prototipo\Images\menu_background.jpg'), (self.__width, self.__height))
+
+        @property
+        def height(self):
+            return self.__height
+
+        @height.setter
+        def height(self, height):
+            self.__height = height
+
+        @property
+        def width(self):
+            return self.__width
+
+        @width.setter
+        def width(self, width):
+            self.__width = width
+
+        @property
+        def display(self):
+            return self.__display
