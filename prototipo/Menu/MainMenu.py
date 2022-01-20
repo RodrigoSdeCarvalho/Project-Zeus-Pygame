f414ddd9627d21d08730bd4d193de5244234fb55
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2022-01-20 16:52:29 -0300

    Criação da classe MainMenu (métodos ainda não funcionais)

---
+++ prototipo/Menu/MainMenu.py
@@ -0,0 +1,24 @@
+import pygame
+
+class MainMenu:
+    def __init__(self, display):
+        self.__buttons = []
+        self.__menu_background = pygame.image.load("prototipo\Images\menu_background.jpg")
+        self.__display = display
+
+    def open_play(self):
+        #play = Play()
+        pass
+
+    def open_help(self):
+        #help = Help()
+        pass
+
+    def open_settings(self):
+        #settings = Settings()
+        pass
+
+    def show_main_menu(self):
+        while True:
+            #display.blit(self.__menu_background, (0, 0))
+            pygame.display.update()
