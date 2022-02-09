1b16dc2142ad6e3268faa1e712141a2171161f8a
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2022-02-09 04:48:26 -0300

    methods that find the current stage and start it

--- prototipo/Menu/Play.py
+++ prototipo/Menu/Play.py
@@ -1,10 +1,30 @@
 import pygame
+
 from Menu.Button import Button
+from GameLogic.Stage.Stage import Stage
 
 class Play(Button):
-    def __init__(self, text: str, width: int, height: int, x: int, y: int, elevation: int, topColor: str, bottomColor: str):
+    def __init__(self, text: str, width: int, height: int, x: int, y: int, elevation: int, topColor: str, bottomColor: str, surface):
         super().__init__(text, width, height, x, y, elevation, topColor, bottomColor)
-        self.screen = pygame.display.get_surface()
+        #self.screen = pygame.display.get_surface()
+        self.__stage_list = [Stage(1, 1, surface, False)]
+        self.window = surface
+
+    @property
+    def stage_list(self):
+        return self.__stage_list
+
+    def current_stage(self):
+        for stage in self.stage_list:
+            if stage.stage_completed == False:
+                print(stage.level)
+                return stage
+
+    def start_current_stage(self):
+        current_stage = self.current_stage()
+        current_stage.start()
 
     def onClick(self):
-        self.screen.fill('#ffffff')
+            self.window.display.fill('#ffffff') #Retirar depois 
+            self.current_stage()
+            self.start_current_stage()
