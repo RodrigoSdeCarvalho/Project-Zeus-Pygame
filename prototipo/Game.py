c34d24800b8911508bea96de5150634026b4ad3b
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2022-03-17 17:08:47 -0300

    Implemented a background song to the game

--- prototipo/Game.py
+++ prototipo/Game.py
@@ -1,12 +1,18 @@
 import pygame
 from GameLogic.GUI.GameDisplay import GameDisplay
 from Menu.MainMenu import MainMenu
+from pygame.locals import *
+from pygame import mixer
 
 class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Project Zeus")
         pygame.display.set_icon(pygame.transform.scale(pygame.image.load('prototipo/Images/game_icon.png'), (75,75)))
+        
+        mixer.init()
+        mixer.music.load("prototipo\Sons\song.ogg")
+        mixer.music.play()
 
         fps = 60
         self.clock = pygame.time.Clock()
